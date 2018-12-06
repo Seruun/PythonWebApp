58dae445d75ace19d9e2b03ba7d44b1f8fcf7d58
Christopher Hübner <marcuste1991@gmail.com>
2018-12-07 00:09:16 +0100

    Main config.py added

---
+++ config.py
@@ -0,0 +1,45 @@
+# config.py
+
+
+class Config(object):
+    """
+    Common configurations
+    """
+
+    # Put any configurations here that are common across all environments
+
+
+class DevelopmentConfig(Config):
+    """
+    Development configurations
+    """
+
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    """
+    Production configurations
+    """
+
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestingConfig(Config):
+    """
+    Testing configurations
+    """
+
+    TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig,
+    'default': ProductionConfig
+}
